bb516d09a4eaee2b832558441a9b94fa538dc930
mdabir1203 <mdabir1203@gmail.com>
2024-07-11 14:56:32 +0500

    setup retro game

---
+++ RetroSpy/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RetroSpyGame from './components/RetroSpyGame';
+
+function App() {
+  return (
+    <div className="App">
+      <RetroSpyGame />
+    </div>
+  );
+}
+
+export default App;
